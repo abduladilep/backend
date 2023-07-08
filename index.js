824838b101bb051cd781cbb600d7afb25b8ef7ce
abduladilep <111062524+abduladilep@users.noreply.github.com>
2023-07-09 00:18:29 +0530

    accounphoto download progresst

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv=require("dotenv");
+// const multer = require('multer');
+// const sharp = require('sharp');
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage });
+// const JSZip = require('jszip');
+
 
 dotenv.config()
 
@@ -14,6 +20,43 @@ app.use(express.json());
 
 
 app.use('/api/user',require('./Router/UserRouter'))
+
+
+// app.post('/api/convert', upload.array('images'), async (req, res) => {
+//   try {
+//     const convertedFiles = [];
+//     console.log("popodowlodd");
+
+//     // Loop through each uploaded file
+//     for (const file of req.files) {
+//       console.log("popopooooooooooooooo");
+//       // Convert the file to the desired format (e.g., JPEG)
+//       const convertedBuffer = await sharp(file.buffer).jpeg().toBuffer();
+      
+//       // Generate a unique filename for the converted file
+//       const convertedFilename = `${file.originalname.split('.')[0]}.jpg`;
+
+//       // Add the converted file to the array
+//       convertedFiles.push({ filename: convertedFilename, buffer: convertedBuffer });
+//     }
+
+//     // Set the response headers for file download
+//     res.set('Content-Type', 'application/zip');
+//     res.set('Content-Disposition', 'attachment; filename=converted_images.zip');
+
+//     // Create a zip file containing the converted files and send it to the client
+//     const zip = new JSZip();
+//     for (const file of convertedFiles) {
+//       zip.file(file.filename, file.buffer);
+//     }
+//     const zipBlob = await zip.generateAsync({ type: 'nodebuffer' });
+//     res.send(zipBlob);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('An error occurred during image conversion');
+//   }
+// });
+
 
 
 mongoose
